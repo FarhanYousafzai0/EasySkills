e6480d75c461382952705bffdb4138b1cd7efba2
Farhan Yousafzai <coderwithferry@gmail.com>
2025-10-08 01:10:58 +0500

    Latest

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-     <Nav/>
-    </div>
+    <div className="min-h-screen max-w-screen relative">
+    {/* Radial Gradient Background from Top */}
+    <div
+      className="absolute inset-0 z-0"
+      style={{
+        background: "radial-gradient(125% 125% at 50% 10%, #fff 40%, #475569 100%)",
+      }}
+    />
+   <Nav/>
+ 
+  </div>
   )
 }
 
